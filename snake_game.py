b546d2a4c408aa6c260d73f4a4f805b658f8fe15
ishworkhanal21 <ishworkhanal21@gmail.com>
2019-12-13 21:29:51 +0545

    added the move function

--- snake_game.py
+++ snake_game.py
@@ -13,5 +13,26 @@ head.speed(0)
 head.shape('square')
 head.color('white')
 head.penup()
-head.goto(0,100)
+head.goto(0,0)
 head.direction = 'stop'
+
+#function to move snake
+def move():
+    if head.direction == 'up':
+        y = head.ycor()
+        head.sety(y+20)
+    if head.direction  == 'down':
+        y = head.ycor()
+        head.sety(y-20)
+    if head.direction == 'right':
+        x = head.xcor()
+        head.setx(x+20)
+    if head.direction == 'left':
+        x = head.xcor()
+        head.setx(x-20)
+
+
+#main game loop 
+while True:
+    win.update()
+    move()
