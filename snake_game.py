2954f5efb7615f3993a248c879e34f614110b445
ishworkhanal21 <ishworkhanal21@gmail.com>
2019-12-13 21:22:34 +0545

    added snake head

--- snake_game.py
+++ snake_game.py
@@ -6,3 +6,12 @@ win.title('Snake Game')
 win.bgcolor('black')
 win.setup(width=600,height=600)
 win.tracer(0)
+
+#setup the snake head
+head = turtle.Turtle()
+head.speed(0)
+head.shape('square')
+head.color('white')
+head.penup()
+head.goto(0,100)
+head.direction = 'stop'
